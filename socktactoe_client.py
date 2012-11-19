f3a9da576578519e9e41b3ced38af4e4bed14b57
Allison Kaptur <allison.kaptur@gmail.com>
2012-11-19 13:47:13 -0500

    fixed command-line host parsing

--- socktactoe_client.py
+++ socktactoe_client.py
@@ -3,7 +3,7 @@ import sys
 import pdb
 
 if __name__ == '__main__':
-    HOST = sys.argv.pop() if len(sys.argv) == 3 else "127.0.0.1"
+    HOST = sys.argv.pop() if len(sys.argv) == 2 else "127.0.0.1"
     PORT = 1060
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
@@ -19,7 +19,12 @@ if __name__ == '__main__':
             s.sendall(msg_to_send)
             msg_recv = s.recv(100)
 
+        if not msg_recv:
+            print "Sorry, something went wrong."
+            break
+
         print msg_recv
+
         if "Game over" in msg_recv:
             print "Thanks for playing!"
             break
--- socktactoe_server.py
+++ socktactoe_server.py
@@ -18,7 +18,6 @@ def get_message(sock):
     try:
         some_string = sock.recv(128)  # move is only three bytes, but want to catch all of invalid move at once
         print "move received:", some_string
-        pdb.set_trace()
         return some_string
     except socket.error as e:
         print e
@@ -26,7 +25,7 @@ def get_message(sock):
 
 
 def make_listen_sock():
-    HOST = sys.argv.pop() if len(sys.argv) == 3 else ''
+    HOST = sys.argv.pop() if len(sys.argv) == 2 else ""
     PORT = 1060
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -62,16 +61,14 @@ if __name__ == '__main__':
     # Form: {File_number: (socket, game, message)}
 
     while True:
-        # print "Ready to enter game loop"
-        # pdb.set_trace()
-
+        #######################
         ### GAME PROCESSING ###
+        #######################
         closed_socks = []
         for id_num in game_sockets.keys():
             sock, game, msg_queued, close_socket = game_sockets[id_num]
 
             if close_socket and not msg_queued:
-                print sock, sock.fileno()
                 sock.close()
                 closed_socks.append(id_num)
 
@@ -80,7 +77,6 @@ if __name__ == '__main__':
                 close_socket = True
 
             elif game.player == 'o':
-                print "running minimax"
                 game.minimax()
                 if game.is_over():
                     msg_queued = game.board_as_string()+'\n'+game.end_message()
@@ -89,13 +85,14 @@ if __name__ == '__main__':
                     msg_queued = game.board_as_string()
                 game.player = 'x'
 
-
             game_sockets[id_num] = [sock, game, msg_queued, close_socket]
 
         for id_num in closed_socks:
             del game_sockets[id_num]
 
+        #########################
         ### SOCKET PROCESSING ###
+        #########################
         all_sockets = game_sockets.keys() + [listen_id]
         print "All sockets:", all_sockets
 
@@ -103,9 +100,6 @@ if __name__ == '__main__':
         print "Read:", read_socks, '\nWrite:', write_socks
         #  Perhaps surprising that this works, since all_sockets is just a list of filenumbers.
         #  However, select.select is just looking for file numbers and looking up the corresponding socket objects.
-
-        # print "Ready to enter socket loops"
-        # pdb.set_trace()
 
         for id_num in read_socks:
             if id_num == listen_id:
@@ -123,10 +117,7 @@ if __name__ == '__main__':
                 sock, game, _, _ = game_sockets[id_num]
                 if game.player == 'x':
                     handle_client_move(id_num, game_sockets)
-                else:
-                    pass
-                    # print "Client message received at wrong time"
-                    # sys.exit()  # this is not how you do error handling
+
 
         for id_num in write_socks:
             sock, _, message, _ = game_sockets[id_num]
